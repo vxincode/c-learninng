37c695458fcff614b79df2ec04ff90d7f8d174a5
chen <1131596911@qq.com>
2025-03-27 00:27:28 +0800

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// //使用System名字的命名空间
+// using System;
+// //定义命名空间
+// namespace ConsoleApp1;
+// //class声明类Program其中包含了方法和数据
+// class Program
+// {
+//     //main方法，程序的入口
+//     static void Main(string[] args)
+//     {
+//         //main方法通过语句来定义行为，进行具体操作和逻辑
+//         Console.WriteLine("Hello, World!");
+//     }
+// }
